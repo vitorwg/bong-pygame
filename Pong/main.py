8ce0fc397ba89222a8ca6d0de431bb3186914f44
vitorwg <vitorwg96@gmail.com>
2022-03-04 15:20:09 -0300

    Added a paddle and configuration of the draw objects in the game

--- Pong/main.py
+++ Pong/main.py
@@ -2,15 +2,57 @@ import pygame
 
 pygame.init()
 
+# Game window config
 WIDTH, HEIGHT = 700, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong")
 
 
+# Filds
+FPS = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
+
+class Paddle:
+    COLOR = WHITE
+
+    # Inicialize the paddle parameters
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    # Draw the paddle as a rectangle
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
+
+
+# Draw the objects in the game
+def draw(win, paddles):
+    win.fill(BLACK)
+
+    for paddle in paddles:
+        paddle.draw(win)
+
+    pygame.display.update()
+
+
+# Lopping the game
 def main():
     update = True
+    clock = pygame.time.Clock()
+
+    left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
 
     while update:
+        clock.tick(FPS)
+        draw(WIN, [left_paddle, right_paddle])
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 update = False
